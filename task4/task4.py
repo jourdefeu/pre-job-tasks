cdd670903bd776428be3357f1bd1c040c97eff59
Valerie <157377780+jourdefeu@users.noreply.github.com>
2024-08-29 10:07:45 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,21 @@
-# task4
+user_input = input("Введите числа через пробел: ")
+numbers = list(map(int, user_input.split()))
+
+def calculate_minimum_moves(nums):
+    nums.sort()  # сортируем массив для поиска медианы
+    n = len(nums)
+
+    # находим медиану
+    if n % 2 == 1:
+        median = nums[n // 2]
+    else:
+        median = (nums[n // 2 - 1] + nums[n // 2]) // 2
+
+    # считаем количество ходов
+    moves = sum(abs(x - median) for x in nums)
+
+    return moves
+
+result = calculate_minimum_moves(numbers)
+
+print(f"Минимальное количество ходов: {result}")
